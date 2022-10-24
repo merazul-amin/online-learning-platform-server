3a56fbd304278ccd0d5e23d584b3882bd898bdca
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-24 22:34:00 +0600

    couses api build

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.Port || 5000;
+const cors = require('cors');
 app.use(cors());
 const allCourses = require('./data/courses.json');
+app.get('/', (req, res) => {
+    res.send('Server Running')
+})
 
 app.get('/courses', (req, res) => {
-
+    res.send(allCourses);
+})
+app.listen(port, () => {
+    console.log('server is running')
 })
