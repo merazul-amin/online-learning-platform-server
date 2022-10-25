338fcc13583e5dc44fe75d4d11a1ad3f136ed47c
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-25 16:04:24 +0600

    course details added

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const port = process.env.Port || 5000;
 const cors = require('cors');
 app.use(cors());
 const allCourses = require('./data/courses.json');
+const courseDetails = require('./data/course-details.json');
 app.get('/', (req, res) => {
     res.send('Server Running')
 })
 
 app.get('/courses', (req, res) => {
     res.send(allCourses);
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourse = courseDetails.find(c => c.id == id);
+    res.send(singleCourse);
 })
 app.listen(port, () => {
     console.log('server is running')
